f32985dda8ca8e4e24d25755e75de4dbf52be8f5
Andrea Huber <ah@picturepark.com>
2019-05-21 16:59:49 +0200

    Proceeded rework

--- Picturepark.PressPortal.Demo/Picturepark.PressPortal.Demo/wwwroot/resources/functions/functions.js
+++ Picturepark.PressPortal.Demo/Picturepark.PressPortal.Demo/wwwroot/resources/functions/functions.js
@@ -16,31 +16,49 @@ var ScrollPosition = /** @class */ (function () {
 }());
 var ArticleHandler = /** @class */ (function () {
     function ArticleHandler(data) {
-        this.articleCount = data.articleCount;
-        this.pageCount = data.pageCount;
-        this.article = data.article;
-        this.pageId = data.pageId;
-        this.pageName = data.pageName;
-        this.currentPage = data.currentPage;
+        this.article = document.getElementsByClassName("article-box");
+        this.articleCount = document.getElementsByClassName("article-box").length;
+        this.pageCount = Math.ceil(document.getElementsByClassName("article-box").length / 9);
+        this.pageId = [];
+        this.pageName = [];
+        for (var i = 0; i < this.articleCount; i++) { //check current page
+            if (!this.article[i].classList.contains("hide-element")) {
+                this.currentPage = parseInt(this.article[i].className[this.article[i].className.length - 1]);
+            }
+        }
+        this.pageList = document.getElementsByClassName("page-list")[0];
     }
     return ArticleHandler;
 }());
 //article overflow handler
 loadElement();
 function overflowHanlder() {
-    var articleHandler = new ArticleHandler({
-        articleCount: document.getElementsByClassName("article-box").length,
-        pageCount: Math.ceil(document.getElementsByClassName("article-box").length / 9),
-        article: document.getElementsByClassName("article-box"),
-        pageId: [],
-        pageName: [],
-        currentPage: 1
-    });
+    var articleHandler = new ArticleHandler({});
     if (articleHandler.articleCount > 9) {
         for (var i = 0; i < articleHandler.pageCount; i++) { //fill array with pages
             articleHandler.pageId.push(i + 1);
             articleHandler.pageName.push("pageNr" + (i + 1));
+            //create page numbers
+            var node = document.createElement("div");
+            node.setAttribute("onclick", "changePage(this.id)");
+            node.classList.add("page-number");
+            node.id = "page-nav-nr" + (i + 1);
+            node.innerHTML = (i + 1).toString();
+            articleHandler.pageList.append(node);
         }
+        articleHandler.pageCount = 12;
+        if (articleHandler.pageCount > 4) { //only display so many page numbers
+            for (var i = 4; i < articleHandler.pageCount; i++) {
+                document.getElementsByClassName("page-number")[i].classList.add("hide-element");
+                console.log(i);
+            }
+            //make this function dynamic
+            var node = document.createElement("div");
+            node.classList.add("page-number");
+            node.innerHTML = "...";
+            articleHandler.pageList.append(node);
+        }
+        //give current page class "active" or skomething like that
         //assign articles to pages
         var para1 = 0;
         var para2 = 9;
@@ -64,6 +82,43 @@ function overflowHanlder() {
         document.getElementsByClassName("last-page")[0].classList.add("hide-element");
     }
 }
+function changePage(clickedPage) {
+    var articleHandler = new ArticleHandler({});
+    switch (clickedPage) {
+        case "nextPageBtn":
+            articleHandler.currentPage = articleHandler.currentPage + 1;
+            break;
+        case "lastPageBtn":
+            articleHandler.currentPage = articleHandler.currentPage - 1;
+            break;
+        default:
+            articleHandler.currentPage = parseInt(clickedPage[clickedPage.length - 1]);
+            break;
+    }
+    for (var i = 0; i < articleHandler.articleCount; i++) {
+        if (articleHandler.article[i].classList.contains("pageNr" + articleHandler.currentPage)) {
+            articleHandler.article[i].classList.remove("hide-element");
+        }
+        else {
+            articleHandler.article[i].classList.add("hide-element");
+        }
+    }
+    window.scroll({ top: 0, behavior: 'smooth' });
+    switch (articleHandler.currentPage) { //hide and display page nav btns
+        case 1:
+            document.getElementsByClassName("next-page")[0].classList.remove("hide-element");
+            document.getElementsByClassName("last-page")[0].classList.add("hide-element");
+            break;
+        case articleHandler.pageCount:
+            document.getElementsByClassName("next-page")[0].classList.add("hide-element");
+            document.getElementsByClassName("last-page")[0].classList.remove("hide-element");
+            break;
+        default:
+            document.getElementsByClassName("next-page")[0].classList.remove("hide-element");
+            document.getElementsByClassName("last-page")[0].classList.remove("hide-element");
+            break;
+    }
+}
 function loadElement() {
     if (document.getElementsByClassName("article-box").length == 0) {
         window.requestAnimationFrame(loadElement);
@@ -72,22 +127,20 @@ function loadElement() {
         overflowHanlder();
     }
 }
-//button function
 function clickOpenBtn(btnEl, childEl) {
     var button = new Button({
         id: btnEl,
         content: childEl
     });
-    button.content.style.display = "block";
+    button.content.classList.remove("hide-element");
     document.addEventListener("click", function () {
-        button.content.style.display = "none";
+        button.content.classList.add("hide-element");
     });
     button.id.addEventListener("click", function (ev) {
-        button.content.style.display = "block";
+        button.content.classList.remove("hide-element");
         ev.stopPropagation();
     });
 }
-//header white on scroll
 window.addEventListener("scroll", function () {
     var scrollPosition = new ScrollPosition({
         posY: window.scrollY,
@@ -102,6 +155,5 @@ window.addEventListener("scroll", function () {
         scrollPosition.element.classList.remove("header-scroll-position");
     }
 });
-//activate search on search button click
 //toggle mobile navigation
 //# sourceMappingURL=functions.js.map
--- Picturepark.PressPortal.Demo/Picturepark.PressPortal.Demo/wwwroot/resources/functions/functions.ts
+++ Picturepark.PressPortal.Demo/Picturepark.PressPortal.Demo/wwwroot/resources/functions/functions.ts
@@ -23,20 +23,27 @@ class ScrollPosition {
 }
 
 class ArticleHandler {
+    article: HTMLCollection;
     articleCount: number;
     pageCount: number;
-    article: HTMLCollection;
     pageId: number[];
     pageName: string[];
     currentPage: number;
+    pageList: HTMLElement;
 
     constructor(data?: any) {
-        this.articleCount = data.articleCount;
-        this.pageCount = data.pageCount;
-        this.article = data.article as HTMLCollection;
-        this.pageId = data.pageId;
-        this.pageName = data.pageName;
-        this.currentPage = data.currentPage;
+        this.article = document.getElementsByClassName("article-box") as HTMLCollection;
+        this.articleCount = document.getElementsByClassName("article-box").length as number;
+        this.pageCount = Math.ceil(document.getElementsByClassName("article-box").length as number / 9);
+        this.pageId = [];
+        this.pageName = [];
+        for (let i = 0; i < this.articleCount; i++) { //check current page
+            if (!this.article[i].classList.contains("hide-element")) {
+                this.currentPage = parseInt(this.article[i].className[this.article[i].className.length - 1]);
+            }
+        }
+
+        this.pageList = document.getElementsByClassName("page-list")[0] as HTMLElement;
     }
 }
 
@@ -44,20 +51,38 @@ class ArticleHandler {
 loadElement();
 
 function overflowHanlder() {
-    const articleHandler = new ArticleHandler({
-        articleCount: document.getElementsByClassName("article-box").length as number,
-        pageCount: Math.ceil(document.getElementsByClassName("article-box").length as number / 9),
-        article: document.getElementsByClassName("article-box"),
-        pageId: [],
-        pageName: [],
-        currentPage: 1
-    });
+    const articleHandler = new ArticleHandler({});
 
     if (articleHandler.articleCount > 9) {
         for (let i = 0; i < articleHandler.pageCount; i++) { //fill array with pages
             articleHandler.pageId.push(i + 1);
             articleHandler.pageName.push("pageNr" + (i + 1));
+
+            //create page numbers
+            let node: HTMLElement = document.createElement("div");
+            node.setAttribute("onclick", "changePage(this.id)");
+            node.classList.add("page-number");
+            node.id = "page-nav-nr" + (i + 1);
+            node.innerHTML = (i + 1).toString();
+            articleHandler.pageList.append(node);
         }
+
+
+        articleHandler.pageCount = 12;
+        if (articleHandler.pageCount > 4) { //only display so many page numbers
+            for (let i = 4; i < articleHandler.pageCount; i++) {
+                document.getElementsByClassName("page-number")[i].classList.add("hide-element");
+                console.log(i);
+            }
+
+            //make this function dynamic
+            let node: HTMLElement = document.createElement("div");
+            node.classList.add("page-number");
+            node.innerHTML = "...";
+            articleHandler.pageList.append(node);
+        }
+        //give current page class "active" or skomething like that
+
 
         //assign articles to pages
         let para1: number = 0;
@@ -84,6 +109,48 @@ function overflowHanlder() {
     }
 }
 
+function changePage(clickedPage) { //page navigation
+    const articleHandler = new ArticleHandler({});
+
+    switch (clickedPage) {
+        case "nextPageBtn":
+            articleHandler.currentPage = articleHandler.currentPage + 1;
+            break;
+        case "lastPageBtn":
+            articleHandler.currentPage = articleHandler.currentPage - 1;
+            break;
+        default:
+            articleHandler.currentPage = parseInt(clickedPage[clickedPage.length - 1]);
+            break;
+    }
+
+    for (let i = 0; i < articleHandler.articleCount; i++) {
+        if (articleHandler.article[i].classList.contains("pageNr" + articleHandler.currentPage)){
+            articleHandler.article[i].classList.remove("hide-element");
+        }
+        else {
+            articleHandler.article[i].classList.add("hide-element");
+        }
+    }
+
+    window.scroll({top: 0, behavior: 'smooth'});
+
+    switch (articleHandler.currentPage) { //hide and display page nav btns
+        case 1:
+            document.getElementsByClassName("next-page")[0].classList.remove("hide-element");
+            document.getElementsByClassName("last-page")[0].classList.add("hide-element");
+            break;
+        case articleHandler.pageCount:
+            document.getElementsByClassName("next-page")[0].classList.add("hide-element");
+            document.getElementsByClassName("last-page")[0].classList.remove("hide-element");
+            break;
+        default:
+            document.getElementsByClassName("next-page")[0].classList.remove("hide-element");
+            document.getElementsByClassName("last-page")[0].classList.remove("hide-element");
+            break;
+    }
+}
+
 function loadElement() { //wait until articles are loaded
     if (document.getElementsByClassName("article-box").length == 0) {
         window.requestAnimationFrame(loadElement); 
@@ -93,27 +160,24 @@ function loadElement() { //wait until articles are loaded
     }
 }
 
-//button function
-function clickOpenBtn(btnEl, childEl) {
+function clickOpenBtn(btnEl, childEl) { //button function
     const button = new Button({
         id: btnEl as HTMLElement,
         content: childEl as HTMLElement
     });
 
-    button.content.style.display = "block";
+    button.content.classList.remove("hide-element");
 
     document.addEventListener("click", function () {
-        button.content.style.display = "none";
+        button.content.classList.add("hide-element");
     });
     button.id.addEventListener("click", function (ev) {
-        button.content.style.display = "block";
+        button.content.classList.remove("hide-element");
         ev.stopPropagation();
     });
 }
 
-
-//header white on scroll
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", function () { //header white on scroll
     const scrollPosition = new ScrollPosition({
         posY: window.scrollY,
         element: document.getElementsByClassName("header-first-row")[0] as HTMLElement
@@ -130,9 +194,4 @@ window.addEventListener("scroll", function () {
 });
 
 
-//activate search on search button click
-
-
 //toggle mobile navigation
-
-
