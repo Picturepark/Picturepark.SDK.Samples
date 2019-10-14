2a7129358cc81d0c7d6b91f04c7b48ad889377d3
Urs Brogle <ub@picturepark.com>
2019-10-14 14:26:43 +0200

    PP9-9359: Update Picturepark SDK and ProxyKit, code cleaup, show content schema for virtual content.

--- Picturepark.ContentPortal.Demo/Picturepark.ContentPortal.Demo/ClientApp/src/app/components/item-details/item-details.component.ts
+++ Picturepark.ContentPortal.Demo/Picturepark.ContentPortal.Demo/ClientApp/src/app/components/item-details/item-details.component.ts
@@ -1,13 +1,10 @@
 import { Component, Input, OnInit, OnDestroy, SecurityContext } from '@angular/core';
 import { LiquidRenderingService } from '@picturepark/sdk-v1-angular-ui';
 import {
-  ContentService, ContentDetail, ContentResolveBehavior,
-  ContentType, ContentDownloadLinkCreateRequest, ContentDownloadRequestItem, SchemaService, SchemaDetail
+  ContentService, ContentDetail, ContentResolveBehavior, ContentType, SchemaService, SchemaDetail
 } from '@picturepark/sdk-v1-angular';
-import { SafeUrl, DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { Item } from '../../models/item.model';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Subscription } from 'rxjs';
-import { TranslationService } from '../../services/translation.service';
 
 @Component({
   selector: 'app-item-details',
@@ -20,18 +17,7 @@ export class ItemDetailsComponent implements OnInit, OnDestroy {
 
   public loading = true;
 
-  public item: Item = null;
-
-  public imageUrl: SafeUrl | null = null;
-
   public content: ContentDetail = null;
-
-  public virtualItemHtml: SafeHtml | null = null;
-
-  public outputFormats: {
-    outputFormatId: string;
-    name: string;
-  }[];
 
   public schemas: SchemaDetail[];
 
@@ -40,90 +26,28 @@ export class ItemDetailsComponent implements OnInit, OnDestroy {
   constructor(
     private contentService: ContentService,
     private schemaService: SchemaService,
-    private translationService: TranslationService,
     private liquidRenderingService: LiquidRenderingService,
     private sanitizer: DomSanitizer) {
   }
 
-  public openContent() {
-    const isPdf = this.content.contentType === ContentType.InterchangeDocument || this.content.contentType === ContentType.Presentation;
-    const isAudio = this.content.contentType === ContentType.Audio;
-    const isVideo = this.content.contentType === ContentType.Video;
-
-    const isMovie = isAudio || isVideo;
-    const isImage = !isMovie && !isPdf;
-
-    const previewOutput =
-      isPdf ? this.content.outputs.filter(o => o.outputFormatId === 'Pdf')[0] :
-        isAudio ? this.content.outputs.filter(o => o.outputFormatId === 'AudioSmall')[0] :
-          isVideo ? this.content.outputs.filter(o => o.outputFormatId === 'VideoSmall')[0] :
-            this.content.outputs.filter(o => o.outputFormatId === 'Preview')[0];
-
-    const request = new ContentDownloadLinkCreateRequest({
-      contents: [
-        new ContentDownloadRequestItem({
-          contentId: this.itemId,
-          outputFormatId: previewOutput.outputFormatId
-        })
-      ]
-    });
-
-    const linkSubscription = this.contentService.createDownloadLink(request).subscribe(response => {
-      const item = {
-        id: this.itemId,
-        isPdf: isPdf,
-        isImage: isImage,
-        isMovie: isMovie,
-        isBinary: this.content.contentType !== ContentType.Virtual,
-        displayValues: {},
-        previewUrl: isImage ? response.downloadUrl : this.imageUrl,
-        originalUrl: response.downloadUrl,
-        originalFileExtension: previewOutput.detail.fileExtension,
-        detail: {
-          width: (<any>previewOutput.detail).width,
-          height: (<any>previewOutput.detail).height,
-        }
-      };
-
-      ((<any>window).pictureparkWidgets).players.showDetailById(item.id, [item]);
-    });
-
-    this.subscription.add(linkSubscription);
-  }
-
   public ngOnInit() {
-    const contentSubscription = this.contentService.get(this.itemId,
-      [ContentResolveBehavior.Metadata,
-      ContentResolveBehavior.LinkedListItems,
-      ContentResolveBehavior.Outputs,
-      ContentResolveBehavior.OuterDisplayValueName,
-      ContentResolveBehavior.InnerDisplayValueName,
-      ContentResolveBehavior.InnerDisplayValueList,
-      ContentResolveBehavior.InnerDisplayValueThumbnail]).subscribe(async content => {
+    const contentSubscription = this.contentService.get(
+      this.itemId,
+      [
+        ContentResolveBehavior.Content,
+        ContentResolveBehavior.Metadata,
+        // ContentResolveBehavior.LinkedListItems,
+        ContentResolveBehavior.Outputs,
+        ContentResolveBehavior.OuterDisplayValueName,
+        ContentResolveBehavior.OuterDisplayValueDetail,
+        ContentResolveBehavior.InnerDisplayValueName,
+        ContentResolveBehavior.InnerDisplayValueList,
+        ContentResolveBehavior.InnerDisplayValueThumbnail
+      ]).subscribe(async content => {
         await this.liquidRenderingService.renderNestedDisplayValues(content);
         this.content = content;
-        this.item = this.processContent(content);
 
-        if (this.content.contentType === ContentType.Virtual) {
-          this.virtualItemHtml = this.sanitizer.sanitize(SecurityContext.HTML, this.content.displayValues['detail']);
-        } else {
-          this.contentService.download(this.itemId, 'Preview', undefined, undefined, null).subscribe((fileResult) => {
-            this.imageUrl = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(fileResult.data));
-          });
-        }
-
-        this.translationService.getOutputFormatTranslations().then(translations =>
-          this.outputFormats = content.outputs && content.outputs.map(o => {
-            return {
-              outputFormatId: o.outputFormatId,
-              name: translations[o.outputFormatId]
-            };
-          })
-        );
-
-        this.schemaService.getMany(this.content.layerSchemaIds.concat(this.content.contentSchemaId)).toPromise().then(t => {
-          this.schemas = t;
-        });
+        this.schemas = await this.schemaService.getMany(this.content.layerSchemaIds.concat(this.content.contentSchemaId)).toPromise();
         this.loading = false;
       });
 
@@ -134,11 +58,5 @@ export class ItemDetailsComponent implements OnInit, OnDestroy {
     if (this.subscription) {
       this.subscription.unsubscribe();
     }
-  }
-
-  private processContent(content: ContentDetail): Item {
-    return {
-      name: content.displayValues.name
-    };
   }
 }
