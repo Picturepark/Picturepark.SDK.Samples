8bea84b75ee9aed47d74b63ad1c0a1e32c2e4144
tc4rdpp <tc@picturepark.com>
2020-04-27 16:48:45 +0100

    PP9-10776 - Content Portal: Dashboard: Exception when user has too much permissions
     - Fixed error when dashboard item doesn't have thumbnail
     - Translated dashboard item title and description

--- Picturepark.ContentPortal.Demo/Picturepark.ContentPortal.Demo/ClientApp/src/app/models/dashboard-item.model.ts
+++ Picturepark.ContentPortal.Demo/Picturepark.ContentPortal.Demo/ClientApp/src/app/models/dashboard-item.model.ts
@@ -1,6 +1,8 @@
+import { TranslatedStringDictionary } from '@picturepark/sdk-v1-angular';
+
 export interface DashboardItem {
   path: string;
   imageId: string;
-  title: string;
-  description: string;
+  title: TranslatedStringDictionary;
+  description: TranslatedStringDictionary;
 }
