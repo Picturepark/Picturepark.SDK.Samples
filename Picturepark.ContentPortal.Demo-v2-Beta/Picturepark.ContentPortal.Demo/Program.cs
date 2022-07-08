2ec057031f0723e496b90cb9e77a6b6f7e0d2f7c
Urs Brogle <ub@picturepark.com>
2022-07-08 15:18:47 +0200

    PP9-16645: Fix logging

--- Picturepark.ContentPortal.Demo-v2-Beta/Picturepark.ContentPortal.Demo/Program.cs
+++ Picturepark.ContentPortal.Demo-v2-Beta/Picturepark.ContentPortal.Demo/Program.cs
@@ -1,5 +1,7 @@
+using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Serilog;
 
@@ -14,6 +16,18 @@ namespace Picturepark.ContentPortal.Demo
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(
+                    (hostBuilderContext, app) =>
+                    {
+                        app.SetBasePath(File.Exists("/etc/cp/appsettings.json")
+                            ? "/etc/cp"
+                            : Directory.GetCurrentDirectory());
+                        app.AddJsonFile("appsettings.json", optional: false);
+                        app.AddJsonFile($"appsettings.{hostBuilderContext.HostingEnvironment.EnvironmentName}.json",
+                            optional: true);
+                        app.AddEnvironmentVariables("CONTENTPORTAL_");
+                        app.AddCommandLine(args);
+                    })
                 .ConfigureLogging(
                     (hostBuilderContext, logging) =>
                     {
