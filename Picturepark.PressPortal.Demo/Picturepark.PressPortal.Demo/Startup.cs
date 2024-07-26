a43180c88cedfb2b2c49a0e62cb292e87438af7a
Urs Brogle <urs.brogle@fotoware.com>
2024-07-26 11:46:46 +0200

    PP9-20529: Update to .NET 6

--- Picturepark.PressPortal.Demo/Picturepark.PressPortal.Demo/Startup.cs
+++ Picturepark.PressPortal.Demo/Picturepark.PressPortal.Demo/Startup.cs
@@ -3,20 +3,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Localization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 using Picturepark.PressPortal.Demo.Configuration;
-using Picturepark.PressPortal.Demo.Controllers;
 using Picturepark.PressPortal.Demo.Helpers;
 using Picturepark.PressPortal.Demo.Repository;
 using Picturepark.PressPortal.Demo.Services;
 using System.Globalization;
-using System.IdentityModel.Tokens.Jwt;
 
 namespace Picturepark.PressPortal.Demo
 {
@@ -25,17 +21,13 @@ namespace Picturepark.PressPortal.Demo
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-
-            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-        }
+       }
 
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddConfiguration<PictureparkConfiguration>(Configuration);
-            var authConfig = services.AddConfiguration<AuthenticationConfiguration>(Configuration);
-            services.AddConfiguration<AuthorizationConfiguration>(Configuration);
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
@@ -54,46 +46,8 @@ namespace Picturepark.PressPortal.Demo
             services.AddLocalization(options => options.ResourcesPath = "Resources");
 
             services.AddControllersWithViews()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
                 .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
                 .AddDataAnnotationsLocalization();
-
-            // Configure authentication
-            services.AddAuthentication("Cookies")
-                .AddCookie("Cookies", options =>
-                {
-                    options.LoginPath = AccountController.LoginPath;
-                    options.AccessDeniedPath = "/account/denied";
-                })
-                .AddOpenIdConnect("oidc", options =>
-                {
-                    options.SignInScheme = "Cookies";
-
-                    options.Authority = authConfig.Authority;
-                    options.ClientId = authConfig.ClientId;
-                    options.ClientSecret = authConfig.ClientSecret;
-                    options.ResponseType = "code id_token";
-
-                    // Development only setting
-                    options.RequireHttpsMetadata = false;
-
-                    options.SaveTokens = true;
-                    options.GetClaimsFromUserInfoEndpoint = true;
-
-                    options.EventsType = typeof(OidcEvents);
-
-                    options.Scope.Clear();
-                    foreach (var scope in authConfig.Scopes)
-                    {
-                        options.Scope.Add(scope);
-                    }
-
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        NameClaimType = "preferred_username",
-                        RoleClaimType = "role"
-                    };
-                });
 
             // Configure supported cultures and localization options
             services.Configure<RequestLocalizationOptions>(options =>
@@ -107,8 +61,6 @@ namespace Picturepark.PressPortal.Demo
                 options.SupportedCultures = supportedCultures;
                 options.SupportedUICultures = supportedCultures;
             });
-
-            services.AddTransient<OidcEvents>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
